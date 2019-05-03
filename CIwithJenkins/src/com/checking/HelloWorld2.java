b355cbb26ac047358ab317f30546f8e7ca27fa74
sandhyarenukarya <sandhya.yelagudige@gmail.com>
2019-05-02 17:00:42 -0700

    Added a print statement

--- CIwithJenkins/src/com/checking/HelloWorld2.java
+++ CIwithJenkins/src/com/checking/HelloWorld2.java
@@ -5,7 +5,7 @@ public class HelloWorld2
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World" + "Doing CI with Github and Jenkins");
-		
+		System.out.println("Printing");
 	}
 
 }
